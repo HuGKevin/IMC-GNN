a4757f569e3ee0becfdca861e02c26481aa8ce11
HuGKevin <kevin.hu@yale.edu>
2021-08-16 10:52:03 -0700

    added early stopping if AUC does not change for specified number of epochs

--- models.py
+++ models.py
@@ -38,7 +38,7 @@ class GCN_Train():
     def __init__(self, 
                  input_dim, output_dim, hidden_channels, 
                  metrics = 'auc', lr = 0.001, loss_fxn = 'cel', optimizer = 'adam',
-                 es_thresh = 0.03, es_lambda = 0.8, es_min_iter = 100):
+                 es_thresh = 0.03, es_lambda = 0.8, es_min_iter = 100, es_stall_limit = 5):
         self.model = GCN(input_dim = input_dim, output_dim = output_dim, hidden_channels = hidden_channels).double()
         self.metric = metrics
         self.flagraiser = False
@@ -50,7 +50,7 @@ class GCN_Train():
         if optimizer == 'adam':
             self.optimizer = torch.optim.Adam(self.model.parameters(), lr = lr)
 
-        self.flag = EarlyStopFlag(es_thresh, es_lambda, es_min_iter)
+        self.flag = EarlyStopFlag(es_thresh, es_lambda, es_min_iter, es_stall_limit)
 
     # Trains model for one epoch
     def train(self, train_DL, verbose = False, ignore_es = False):
@@ -131,7 +131,7 @@ class GCN_Train():
 
 
 class EarlyStopFlag():
-    def __init__(self, threshold, lam, minimum_iters, max_mode = False, name = None):
+    def __init__(self, threshold, lam, minimum_iters, stall_lim, max_mode = False, name = None):
         self.auc_track = []
         self.ema_track = []
         self.count = 0
@@ -139,10 +139,12 @@ class EarlyStopFlag():
         self.__lam__ = lam
         self.__thresh__ = threshold
         self.__minimum_iters__ = minimum_iters
-        self.__max_mode__=max_mode
+        self.__max_mode__ = max_mode
+        self.__stall_lim__ = stall_lim
         self.__name__ = name if name is not None else "Metric." + str(np.random.randint(10000))
 
     def update(self, new_value, verbose = False):
+        # Return True if early stopping is triggered, False if not yet triggered
         if verbose == True:
             print("========Early Stopping========")
 
@@ -163,7 +165,6 @@ class EarlyStopFlag():
             threshold = max(self.ema_track) * (1 - self.__thresh__)
             if verbose == True:
                 print(f"New ema is {new_ema}, while threshold is {threshold}")
-      
 
         self.count += 1
         self.ema_track.append(new_ema)
@@ -172,11 +173,18 @@ class EarlyStopFlag():
             if verbose == True:
                 print("Still under minimum iterations threshold")
             return False
+
+        if stall_test(self.auc_track, self.__stall_lim__):
+            print(f"AUC has not changed over past {self.__stall_lim__} iterations, early stopping triggered.")
+            return True
         
         if new_ema < threshold:
             return True
         else:
             return False
+
+    def stall_test(auc_list, stall_lim):
+        return(len(set(auc_list[-stall_lim:]))) == 1
 
     def reset(self):
         self.auc_track = []
